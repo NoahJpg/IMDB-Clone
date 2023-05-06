b24e5da2a222e03b825c81823f24f33b3aa5468f
Noah Atkinson <noahatkinson1.1@gmail.com>
2023-05-05 21:44:04 -0700

    improve code readability, add card loading from skeleton npm package

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -36,7 +36,7 @@ const Home = () => {
                   <div className='posterImage__runtime'>
                     {movie ? movie.release_date: ""}
                     <span className="posterImage__rating">
-                      {movie ? movie.vote_averge: ""}
+                      {movie ? movie.vote_average: ""}
                       <i className='fas fa-star' /> {" "}
                     </span>
                   </div>
