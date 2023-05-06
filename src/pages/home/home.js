3b7ebd74714013432e409ef3673a72d7036e70c9
Noah Atkinson <noahatkinson1.1@gmail.com>
2023-05-05 21:05:06 -0700

    add carousel from react responsive carousel to loop over movies in api dB and dispaly in rotating carousel

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import './home.css'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+
 
 const Home = () => {
 
@@ -13,7 +16,36 @@ const Home = () => {
 
   return (
     <div>
-      Home Page is now rendered
+      <div className='poster'>
+        <Carousel
+          showThumbs={false}
+          autoPlay={true}
+          transitionTime={3}
+          infiniteLoop={true}
+          showStatus={false}
+        >
+          {
+            popularMovies.map(movie => (
+              <Link style={{textDecoratioin:"none",color:"white"}} to={`movie/${movie.id}`}> 
+                <div className='posterImage'>
+                  <img src={`https://image.tmdb.ort/t/p/original${movie && movie.backdrop_path}`} />
+                </div>
+                <div className='posterImage__overlay'>
+                  <div className='posterImage__title'>{movie ? movie.original_title: ""}</div>
+                  <div className='posterImage__runtime'>
+                    {movie ? movie.release_date: ""}
+                    <span className="posterImage__rating">
+                      {movie ? movie.vote_averge: ""}
+                      <i className='fas fa-star' /> {" "}
+                    </span>
+                  </div>
+                  <div className='posterImage_description'>{movie ? movie.overview : ""}</div>
+                </div>
+              </Link>
+            ))
+          }
+        </Carousel>
+      </div>
     </div>
   )
 }
